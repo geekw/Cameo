f0f1b58c9c49a7a61460bbbdc2a20e21f2f1069c
Robin Wei <weiw@iiseeuu.com>
2013-09-10 16:29:44 +0800

    Implemented enterFrame and exitFrame

--- managers.py
+++ managers.py
@@ -46,3 +46,51 @@ class CaptureManager(object):
 	@property
 	def isWritingVideo(self):
 		return self._imageFilename is not None
+
+	def enterFrame(self):
+		"""Capture the next frame, if any."""
+
+		#But first, check any previous frame has exited.
+		assert not self._enteredFrame, \
+			'Previous enterFrame() had no matching exitFrame()'
+
+		if self._capture is not None:
+			self._enteredFrame = self._capture.grab()
+
+	def exitFrame(self):
+		"""Draw to the window. Write to files. Release the frame."""
+		
+		# Check whether any grabbed frame is retrievable.
+		# The getter may retrieve and cache the frame.
+		if self.frame is None:
+			self._enteredFrame = False
+			return
+
+		# Update the FPS estimate and related variables
+		if self._framesElapsed == 0:
+			self._startTime = time.time()
+		else:
+			timeElapsed = time.time() - self._startTime
+			self._fpsEstimate = self._timeElapsed / timeElapsed
+		self._framesElapsed += 1
+
+		# Draw to the window, if any
+		if self.previewWindowManager is not None:
+			if self.shouldMirrorPreview:
+				mirroredFrame = numpy.fliplr(self._frame).copy()
+				self.previewWindowManger.show(mirroredFrame)
+			else:
+				self.previewWindowManager.show(self._frame) 
+
+		# Write to the image file, if any
+		if self.isWritingImage:
+			cv2.imwrite(self._imageFileName, self._frame)
+			self._imageFileName = None
+
+
+		# Write to the video file, if any
+		self._writeVideoFrame()
+
+		# Release the frame.
+		self._frame = None
+		self._enteredFrame = False
